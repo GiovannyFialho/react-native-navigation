09ac4e4cf8b0b678e0685840dcba8b591b2fe1b7
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-07-03 19:59:56 -0300

    creating a global type for my routes and using in my pages

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,13 +1,15 @@
-import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
+
+import { type StackRoutesListProps } from "@/routes/stack-routes";
 
 import { ButtonIcon } from "@/components/button-icon";
 import { Header } from "@/components/header";
 import { Title } from "@/components/title";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export function Home() {
-  const navigation = useNavigation();
+type HomeProps = NativeStackScreenProps<StackRoutesListProps, "home">;
 
+export function Home({ navigation }: HomeProps) {
   return (
     <View
       style={{ flex: 1, padding: 32, paddingTop: 54, backgroundColor: "#fff" }}
--- src/app/product.tsx
+++ src/app/product.tsx
@@ -1,13 +1,15 @@
-import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
+
+import { StackRoutesListProps } from "@/routes/stack-routes";
 
 import { ButtonIcon } from "@/components/button-icon";
 import { Header } from "@/components/header";
 import { Title } from "@/components/title";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export function Product() {
-  const navigation = useNavigation();
+type ProductProps = NativeStackScreenProps<StackRoutesListProps, "product">;
 
+export function Product({ navigation }: ProductProps) {
   return (
     <View
       style={{ flex: 1, padding: 32, paddingTop: 54, backgroundColor: "#fff" }}
