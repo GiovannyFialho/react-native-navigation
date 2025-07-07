cde8d3c127fafa4a928954efab8cfc623059975d
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-07-07 20:55:33 -0300

    custom bottom tab

--- src/routes/bottom-routes.tsx
+++ src/routes/bottom-routes.tsx
@@ -1,3 +1,4 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import {
   BottomTabScreenProps,
   createBottomTabNavigator,
@@ -20,10 +21,34 @@ export function BottomRoutes() {
   return (
     <Tab.Navigator
       initialRouteName="home"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelPosition: "beside-icon",
+        tabBarActiveTintColor: "#2c46b1",
+        tabBarInactiveTintColor: "#444444",
+      }}
     >
-      <Tab.Screen name="home" component={Home} />
-      <Tab.Screen name="product" component={Product} />
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: "Início",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="product"
+        component={Product}
+        options={{
+          tabBarLabel: "Produto",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="add-circle" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
